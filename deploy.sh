e2c9e3bdc0126987d391a260690305a2d9d0c4d4
Brad Sherman <bsherman1096@gmail.com>
2024-05-16 21:30:15 -0500

    Use rsync to get rid of old files

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ shift $((OPTIND -1))
 # first arg is app
 # second arg is src
 # third arg is dst
-# any other args are passed to 'cp'
+# any other args are passed to 'rsync'
 function copy_config () {
   if [[ $app == "all" || $app == "$1" ]]; then
     echo "Deploying application: $1"
@@ -29,23 +29,23 @@ function copy_config () {
     for var in "${@:4}"; do
         params+=("$var")
     done
-    cp "${params[@]}" "$2" "$3"
+    rsync --recursive --delete-after --inplace "${params[@]}" "$2" "$3"
   else
     echo "Skipping application: $1"
   fi
 }
 
-copy_config "alacritty" ./alacritty ~/.config "-r"
+copy_config "alacritty" ./alacritty ~/.config
 copy_config "display_handler" ./display_handler ~/.local/bin/
-copy_config "dunst" ./dunst ~/.config "-r"
-copy_config "fontconfig" ./fontconfig ~/.config "-r"
+copy_config "dunst" ./dunst ~/.config
+copy_config "fontconfig" ./fontconfig ~/.config
 copy_config "fonts" ./fonts.conf ~/.fonts.conf
 copy_config "gitconfig" ./gitconfig ~/.gitconfig
-copy_config "i3" ./i3 ~/.config "-r"
-copy_config "nvim" ./nvim ~/.config "-r"
-copy_config "polybar" ./polybar ~/.config "-r"
-copy_config "sway" ./sway ~/.config "-r"
-copy_config "waybar" ./waybar ~/.config "-r"
+copy_config "i3" ./i3 ~/.config
+copy_config "nvim" ./nvim ~/.config
+copy_config "polybar" ./polybar ~/.config
+copy_config "sway" ./sway ~/.config
+copy_config "waybar" ./waybar ~/.config
 copy_config "rofi-solarized-light" ./solarized-light.rasi ~/.local/share/rofi/themes/
 copy_config "rofi-nord" ./nord.rasi ~/.local/share/rofi/themes/
 copy_config "tmux" ./tmux.conf ~/.tmux.conf
--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ shift $((OPTIND -1))
 # first arg is app
 # second arg is src
 # third arg is dst
-# any other args are passed to 'cp'
+# any other args are passed to 'rsync'
 function copy_config () {
   if [[ $app == "all" || $app == "$1" ]]; then
     echo "Updating application: $1"
@@ -29,23 +29,23 @@ function copy_config () {
     for var in "${@:4}"; do
         params+=("$var")
     done
-    cp "${params[@]}" "$2" "$3"
+    rsync --recursive --delete-after --inplace "${params[@]}" "$2" "$3"
   else
     echo "Skipping application: $1"
   fi
 }
 
-copy_config "alacritty" ~/.config/alacritty . "-r"
+copy_config "alacritty" ~/.config/alacritty .
 copy_config "display_handler" ~/.local/bin/display_handler .
-copy_config "dunst" ~/.config/dunst . "-r"
-copy_config "fontconfig" ~/.config/fontconfig . "-r"
+copy_config "dunst" ~/.config/dunst .
+copy_config "fontconfig" ~/.config/fontconfig .
 copy_config "fonts" ~/.fonts.conf fonts.conf
 copy_config "gitconfig" ~/.gitconfig gitconfig
-copy_config "i3" ~/.config/i3 . "-r"
-copy_config "nvim" ~/.config/nvim . "-r"
-copy_config "polybar" ~/.config/polybar . "-r"
-copy_config "sway" ~/.config/sway . "-r"
-copy_config "waybar" ~/.config/waybar . "-r"
+copy_config "i3" ~/.config/i3 .
+copy_config "nvim" ~/.config/nvim .
+copy_config "polybar" ~/.config/polybar .
+copy_config "sway" ~/.config/sway .
+copy_config "waybar" ~/.config/waybar .
 copy_config "rofi-solarized-light" ~/.local/share/rofi/themes/solarized-light.rasi .
 copy_config "rofi-nord" ~/.local/share/rofi/themes/nord.rasi .
 copy_config "tmux" ~/.tmux.conf tmux.conf
